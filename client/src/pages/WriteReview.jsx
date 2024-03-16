cc8f7d8756d80632a0bbb75e6f9da5bc5dd05761
sanikashendye <s.shendye02@gmail.com>
2024-03-16 17:19:50 -0600

    updated writereview
    connected to the backend to put review in database - still working on getting the book data - hoping to find a way for isbn to be enough data

--- client/src/pages/WriteReview.jsx
+++ client/src/pages/WriteReview.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
 import '../assets/styling/WriteReview.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { AuthenticationContext, UserContext } from '../context/UserContext.jsx';
+import { useNavigate, Link } from 'react-router-dom';
+
+//component works to get the book data - but need to figure out how to make it accessible in the write review function 
 
 
+// const MyComponent = ({ isbn }) => {
+//     const [bookData, setBookData] = useState(null);
+  
+//     useEffect(() => {
+//       const fetchData = async () => {
+//         try {
+//           const route = `http://localhost:7003/book/getBookByISBN?ISBN=${isbn}`;
+//           const response = await fetch(route, {
+//             method: 'GET',
+//             headers: {
+//               'Access-Control-Allow-Origin': '*',
+//               'Access-Control-Allow-Methods': 'GET',
+//               'Content-Type': 'application/json',
+//             },
+//           });
+  
+//           if (!response.ok) {
+//             throw new Error('Failed to fetch book data');
+//           }
+  
+//           const data = await response.json();
+//           setBookData(data);
+//         } catch (error) {
+//           console.error(error);
+//         }
+//       };
+  
+//       fetchData();
+//     }, [isbn]);
+  
+//     useEffect(() => {
+//       console.log('BOOKDATA', bookData);
+//     }, [bookData]);
+  
+//     return null;
+//   };
+
+
+//hardcoded a book for now
 const book =
     {
         ISBN: '12',
@@ -12,14 +55,22 @@ const book =
 
 export default function WriteReview(){
 
+    const navigate = useNavigate();
+
     const[rating, setRating] = useState(null);
     const[hover, setHover] = useState(null);
     const[totalStars, setTotalStars]=useState(5);
+    const [failText, setFailText] = useState("");
 
-    //need to fetch the username of the logged in user - idk how to do that - could maybe pass as argument?
-    const username = 'username'
+    const { userInfo, setUserInfo } = useContext(UserContext);
+  const { authentication, setAuthentication } = useContext(AuthenticationContext);
+  console.log(userInfo);
 
-    //ALSO NEED TO FETCH SELECTED BOOK ISBN - to get cover and to send to db
+    //works - fetches username from context 
+    const username = userInfo.username;
+
+    //ALSO NEED TO FETCH SELECTED BOOK ISBN - to get cover and to send to db - work in progress
+
 
     const [formData, setFormData] = useState({
         isbn: book.ISBN,
@@ -41,16 +92,43 @@ export default function WriteReview(){
         };
 
 
-    const handleSubmit = (e) =>{
+    async function handleSubmit (e) {
         e.preventDefault();
         // You can handle form submission logic here
-        console.log("FORM SUBMITTED");
-        console.log(formData);
-        //formdata holds the data from the user - can now update/send to database  
+        const body = {
+            ISBN: formData.isbn,
+            username: formData.username,
+            stars: formData.rating, 
+            description: formData.feedback
+        }
+
+        //works - sends data to the db - works as long as the book and username already exists in the db - (foreign keys)
+        const route =  "http://localhost:7003/review/createReview"
+        console.log(route);
+        console.log(JSON.stringify(body))
+        const response = await fetch(route , {method: "POST",
+                                              headers: {"Access-Control-Allow-Origin" : "*",
+                                                        "Access-Control-Allow-Methods" : "POST",
+                                                        "Content-Type" : "application/json"},
+                                                        body: JSON.stringify(body)
+                                    });
+        console.log(response);
+        if(response.status === 400 || response.status === 500 || response.status === 500 || response.status === 500){
+            //need to figure out what to do in terms of error handling
+            setFailText(response.statusText)
+        }
+        else if(response.status == 201){
+            //successfull case 
+            //add navigation to a different page - probably back to philipas page
+
+        }
     };
 
     return(
         <div className ='big-container'>
+            {/* <MyComponent isbn={book.ISBN} /> */}
+            {/* <div>{bookData.author_name}</div> */}
+
             <h1 className="heading">Add Review: New Review</h1>
             <br></br>
         
@@ -63,18 +141,16 @@ export default function WriteReview(){
                 <div className="books-container">
                     <div className="book-card">
                         <img className = "book-cover"src={`/bookCovers/${book.ISBN}.jpg`} alt={book.title} />
+                        
                         {/* <div className="book-info">
                             <div className="title"><h1>{book.title}</h1></div>
                             <div className="author">{book.author}</div>
                         </div> */}
                     </div>
                 </div>
+                {/* <div><h5>{bookCover}</h5></div> */}
+                        
 
-                
-            
-            
-            
-        
             
             </div>
             <div className="panelRight">
@@ -86,7 +162,6 @@ export default function WriteReview(){
                                 <br></br>
                             </h5>
 
-                            {/* /* USER NAME - need to link it to the logged in user */}
                             <div className ="styled-username">{username}</div>
                             
                             
@@ -151,17 +226,10 @@ export default function WriteReview(){
                             ADD REVIEW
                             </button>
                         </div>
-                        
 
                     </form>
 
-                </div>
-
-                {/* REMOVE THIS BUTTON AFTER - JUST HAVE IT RN TO CHECK WHATS IN THE FORM ON THE CONSOLE
-                */}
-                <button onClick={() => console.log(formData)}>Log Form Data</button>
-
-                
+                </div>        
 
             </div>
         </div>
