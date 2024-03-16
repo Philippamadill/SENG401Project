0b73ac961dd62984dc2846278fdd88c75b79e629
NathanRomphf <nathan.romphf@ucalgary.ca>
2024-03-16 11:26:42 -0600

    Add connection to the backend

--- client/src/pages/TopPicks.jsx
+++ client/src/pages/TopPicks.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
-export default function TopPicks(){
-    return(
-        <div>
-            TEMP TEXT
-        </div>
-    )
+export default function TopPicks() {
+  return <div>TEMP TEXT</div>;
 }
