3ac482cdc86600048cf1c7ad836cc03e58f5081d
NathanRomphf <nathan.romphf@ucalgary.ca>
2024-03-17 08:18:36 -0600

    This is the view page from Phillipa's branch, manually copied over cause merging was a headache

---
+++ client/src/pages/ViewBook.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import "../assets/styling/ViewBook.css";
+import { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
+import { IoMdKey } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
+import { useNavigate, Link } from "react-router-dom";
+
+const book = {
+  ISBN: 12,
+  title: "Harry Potter and the Philosopher's Stone",
+  author: "J.K. Rowling",
+  genre: "Fantasy",
+  description:
+    "The first book in the Harry Potter series, this book follows the young wizard Harry Potter as he discovers his magical heritage and begins his journey at Hogwarts School of Witchcraft and Wizardry.",
+  rating: "4.5",
+};
+
+export default function ViewBook(props) {
+  // const[hover, setHover] = useState(null);
+  const ISBN = props.ISBN;
+  const [books, setBooks] = useState();
+
+  async function getBook() {
+    const route = "http://localhost:7003/book/getBookByISBN";
+    console.log(route);
+    const response = await fetch(route, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log("Hello");
+    console.log(response);
+    const jsonResponse = await response.json();
+    setBooks(jsonResponse);
+    console.log(jsonResponse);
+  }
+
+  useEffect(() => {
+    getBook();
+  }, []);
+  return (
+    <div className="ViewBook">
+      <div className="wrapper-view">
+        <div className="book-cover">
+          <img src={`/bookCovers/${book.ISBN}.jpg`} alt={book.title} />
+        </div>
+        <div className="book-info">
+          <h3 className="book-title">{book.title}</h3>
+          <h2 className="book-author">{book.author}</h2>
+          <h4 className="book-genre">Genre: {book.genre}</h4>
+          <h4 className="book-description">Description: {book.description}</h4>
+          <h4 className="book-rating">Rating: {book.rating}</h4>
+        </div>
+        {/* if (loggedIn === true){ */}
+        <div className="book-buttons">
+          <button className="want">Add to "Want to Read"</button>
+          <button className="current">Add to "Currently Reading"</button>
+          <button className="read">Add to "Read"</button>
+          <button className="leave-review">Leave a review</button>
+        </div>
+        {/* } */}
+      </div>
+      <div className="book-reviews">
+        <h6 className="stars">review rating</h6>
+        <div className="review">
+          <p className="review-text">review text</p>
+        </div>
+      </div>
+    </div>
+  );
+}
