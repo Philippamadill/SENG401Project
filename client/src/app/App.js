d426c873e627b8e85741bd61d4cb9a18630f04e5
NathanRomphf <nathan.romphf@ucalgary.ca>
2024-03-16 20:19:19 -0600

    Fix format of the page

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,35 +1,31 @@
-import './App.css';
-import LoginPage from '../pages/Login';
-import RegisterPage from '../pages/Register';
-import ViewBook from '../pages/ViewBook';
-import CurrentlyReading from '../pages/CurrentlyReading';
-import WantToRead from '../pages/WantToRead';
-import AlreadyRead from '../pages/AlreadyRead';
-import TopPicks from '../pages/TopPicks';
-import {BrowserRouter, Route,Routes,ProtectedRoute } from "react-router-dom";
-import Layout from "../Layout"
-
+import "./App.css";
+import LoginPage from "../pages/Login";
+import RegisterPage from "../pages/Register";
+import ViewBook from "../pages/ViewBook";
+import CurrentlyReading from "../pages/CurrentlyReading";
+import WantToRead from "../pages/WantToRead";
+import AlreadyRead from "../pages/AlreadyRead";
+import TopPicks from "../pages/TopPicks";
+import { BrowserRouter, Route, Routes, ProtectedRoute } from "react-router-dom";
+import Layout from "../Layout";
 
 function App() {
-
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element = {<LoginPage />} />
-        <Route path = "/register" element = {<RegisterPage />} />
+        <Route index element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
-        <Routes>
-          <Route path = "/" element = {<Layout />}>\
-            <Route path = "/currentlyreading" element = {<CurrentlyReading />} />
-            <Route path = "/wanttoread" element = {<WantToRead />} />
-            <Route path = "/alreadyread" element = {<AlreadyRead />} />
-            <Route path = "/viewbook" element = {<ViewBook />} />
-            <Route path = "/toppicks" element = {<TopPicks />} />
-          </Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/currentlyreading" element={<CurrentlyReading />} />
+          <Route path="/wanttoread" element={<WantToRead />} />
+          <Route path="/alreadyread" element={<AlreadyRead />} />
+          <Route path="/viewbook" element={<ViewBook />} />
+          <Route path="/toppicks" element={<TopPicks />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-
   );
 }
 
--- server/serviceController.js
+++ server/serviceController.js
@@ -1,13 +1,13 @@
 // packages
-const express = require('express');
-const mysql = require('mysql2');
-const fs = require('fs');
-
+const express = require("express");
+const mysql = require("mysql2");
+const fs = require("fs");
+const cors = require("cors");
 // controllers
-const userController = require('./controllers/user');
-const reviewController = require('./controllers/review');
-const bookController = require('./controllers/book');
-const bookshelfController = require('./controllers/bookshelf');
+const userController = require("./controllers/user");
+const reviewController = require("./controllers/review");
+const bookController = require("./controllers/book");
+const bookshelfController = require("./controllers/bookshelf");
 
 // Run the server
 const app = express();
@@ -15,12 +15,12 @@ const PORT = process.env.PORT || 7003;
 
 // Middleware
 app.use(express.json()); // This line adds JSON parsing middleware
-
+app.use(cors);
 // Routes
-app.use('/user', userController);
-app.use('/review', reviewController);
-app.use('/book', bookController);
-app.use('/bookshelf', bookshelfController);
+app.use("/user", userController);
+app.use("/review", reviewController);
+app.use("/book", bookController);
+app.use("/bookshelf", bookshelfController);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
