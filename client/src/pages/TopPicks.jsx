fd89e67abad4bd4df25e8cd93c4febdbb286aeeb
Philippamadill <philippamadill@gmail.com>
2024-03-20 09:51:40 -0600

    Mayyyyyybeeeeee?????

--- client/src/pages/TopPicks.jsx
+++ client/src/pages/TopPicks.jsx
@@ -4,76 +4,16 @@ import axios from 'axios'; // Assuming you're using Axios for HTTP requests
 import "../assets/styling/TopPick.css";
 import { useNavigate, Link } from "react-router-dom";
 
-// const dummyBooks = [
-//   {
-//     ISBN: "1234567890123",
-//     title: "Book Title 1",
-//     author: "Author 1",
-//   },
-//   {
-//     ISBN: "2345678901234",
-//     title: "Book Title 2",
-//     author: "Author 2",
-//   },
-//   {
-//     ISBN: "1",
-//     title: "Book Title 3",
-//     author: "Author 3",
-//   },
-//   {
-//     ISBN: "1234567890123",
-//     title: "Book Title 1",
-//     author: "Author 1",
-//   },
-//   {
-//     ISBN: "2345678901234",
-//     title: "Book Title 2",
-//     author: "Author 2",
-//   },
-//   {
-//     ISBN: "1",
-//     title: "Book Title 3",
-//     author: "Author2 3",
-//   },
-//   {
-//     ISBN: "1234567890123",
-//     title: "Book Title 1",
-//     author: "Author 1",
-//   },
-//   {
-//     ISBN: "2345678901234",
-//     title: "Book Title 2",
-//     author: "Author 2",
-//   },
-//   {
-//     ISBN: "1",
-//     title: "Book Title 3",
-//     author: "Author 3",
-//   },
-//   // Add more dummy book objects as needed
-// ];
-
-var numArray = [];
-var randomNumber = 0;
-var count = 0;
+var rand = [];
 export default function TopPicks() {
   const [books, setBooks] = useState([]);
   const [randBook, setRandBook] = useState([]);
-  useEffect(() => {
-    async function fetchBooks() {
-      try {
-        const response = await axios.get('http://localhost:7003/getAllBooks'); // Assuming your API endpoint is '/api/books'
-        setBooks(response.data);
-      } catch (error) {
-        console.error("Error fetching books:", error);
-      }
-    }
-    fetchBooks();
-  }, []);
+  getBook();
 
-  async function getrandISBN(){
+
+  /*async function getrandISBN(){
     try {
-      const response = await axios.get('http://localhost:7003/getAllBooks'); 
+      const response = "http://localhost:7003/book/getAllBooks"; 
       count = response.data.count;
     } catch (error) {
       console.error("Error fetching count:", error);
@@ -86,11 +26,18 @@ export default function TopPicks() {
     }
   }
   return randomNumber;
-  }
+  }*/
 
   async function getBook() {
-    const rand = getrandISBN();
     const route = "http://localhost:7003/book/getAllBooks";
+    console.log(route.data);
+    let count = route.data.count;
+    while(rand.length <= 5){
+      let randomNumber = Math.floor(Math.random() * count) + 1; 
+      if (!(rand.includes(randomNumber))){
+        rand.push(randomNumber);
+      }
+    }
     console.log(route);
     const response = await fetch(route, {
       method: "GET",
